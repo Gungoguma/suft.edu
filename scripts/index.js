0765efeceda99d3769409de531b9965677f57422
Gungoguma <khh9550@naver.com>
2020-06-12 22:13:08 +0900

    유소겸 프로젝트 1

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+let index = 1;
+
+function makeChat(name,date,content) {
+    const chatlistDom = document.querySelector(".chatlist");
+    const template = `
+    <div id="char-${index++}">
+            <div class="header">
+                <span class="name">${name}</span>
+                <span class="date">${date}</span>
+            </div>
+            <div class="content">
+                <p>${content}</p>
+            </div>
+        </div>
+        `;
+        chatlistDom.innerHTML += template;
+}
+
+
+const buttonDom = document.querySelector(".input-button");
+const nameDom = document.querySelector(".input-name");
+const contentDom = document.querySelector(".input-content");
+
+function sendChat () {
+    const date = new Date().toLocaleString();
+
+    makeChat(nameDom.value, date, contentDom.value);
+    contentDom.value ="";
+}
+
+buttonDom.addEventListener('click', () => sendChat());
+
+contentDom.addEventListener("keydown", (e) => {
+    if(e.key === "Enter") sendChat();
+});
